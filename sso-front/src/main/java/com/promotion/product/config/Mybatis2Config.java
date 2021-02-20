bf0454cfd131f81cbacb8b4fa8c3f19d6e6c14f2
fantasticL <835899099@qq.com>
2021-02-21 01:45:30 +0800

    更新代码

--- sso-front/src/main/java/com/promotion/product/config/Mybatis2Config.java
+++ sso-front/src/main/java/com/promotion/product/config/Mybatis2Config.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -20,32 +19,29 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 @Component
-@MapperScan(basePackages ="com.promotion.product.dao.mysql", sqlSessionTemplateRef  = "ds1SqlSessionTemplate")
-public class MybatisConfig {
+@MapperScan(basePackages ="com.promotion.product.dao.mysql2", sqlSessionTemplateRef  = "ds2SqlSessionTemplate")
+public class Mybatis2Config {
 
-
-    @Bean("ds1DataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.ds1")
+    @Bean("ds2DataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.ds2")
     public DataSource getDb1DataSource(){
         return DataSourceBuilder.create().build();
     }
 
-    @Primary
-    @Bean("ds1SqlSessionFactory")
-    public SqlSessionFactoryBean createSqlSessionFactory(@Qualifier("ds1DataSource") DataSource dataSource) throws IOException, SQLException {
+    @Bean("ds2SqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactory(@Qualifier("ds2DataSource") DataSource dataSource) throws IOException, SQLException {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         //启用Mybatis的全部xml文件，就不需要一个个去打开
-        String packageSerchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "/mapper/**.xml";
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSerchPath));
+//        String packageSerchPath = PathMatchingResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "/mapper/**.xml";
+//        sqlSessionFactoryBean.setMapperLocations(resolver.getResources(packageSerchPath));
         sqlSessionFactoryBean.setDataSource(dataSource);
         return sqlSessionFactoryBean;
     }
 
 
-    @Primary
-    @Bean(name = "ds1SqlSessionTemplate")
-    public SqlSessionTemplate ds1SqlSessionTemplate(@Qualifier("ds1SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    @Bean(name = "ds2SqlSessionTemplate")
+    public SqlSessionTemplate ds1SqlSessionTemplate(@Qualifier("ds2SqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
--- sso-front/src/main/java/com/promotion/product/controller/IndexController.java
+++ sso-front/src/main/java/com/promotion/product/controller/IndexController.java
@@ -1,22 +1,11 @@
 package com.promotion.product.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.promotion.product.dao.TestDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-
-
-
-    @Autowired
-    private TestDao testDao;
 
 
     @RequestMapping("/add")
