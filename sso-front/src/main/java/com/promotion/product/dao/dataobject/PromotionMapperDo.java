972afe7ce700f1388cdb2b904b57e2a26b441a3d
lilun <lilun_leo@outlook.com>
2021-02-21 16:07:02 +0800

    提交

--- sso-front/src/main/java/com/promotion/product/dao/dataobject/PromotionMapperDo.java
+++ sso-front/src/main/java/com/promotion/product/dao/dataobject/PromotionMapperDo.java
@@ -15,4 +15,90 @@ public class PromotionMapperDo {
     private String  receivingNumber;
     private Boolean deleted;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getActivityCode() {
+        return activityCode;
+    }
+
+    public void setActivityCode(String activityCode) {
+        this.activityCode = activityCode;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRestaurantCode() {
+        return restaurantCode;
+    }
+
+    public void setRestaurantCode(String restaurantCode) {
+        this.restaurantCode = restaurantCode;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getReceivingAccount() {
+        return receivingAccount;
+    }
+
+    public void setReceivingAccount(String receivingAccount) {
+        this.receivingAccount = receivingAccount;
+    }
+
+    public String getReceivingNumber() {
+        return receivingNumber;
+    }
+
+    public void setReceivingNumber(String receivingNumber) {
+        this.receivingNumber = receivingNumber;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "PromotionMapperDo{" +
+                "id=" + id +
+                ", activityCode='" + activityCode + '\'' +
+                ", area='" + area + '\'' +
+                ", city='" + city + '\'' +
+                ", restaurantCode='" + restaurantCode + '\'' +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", receivingAccount='" + receivingAccount + '\'' +
+                ", receivingNumber='" + receivingNumber + '\'' +
+                ", deleted=" + deleted +
+                '}';
+    }
 }
