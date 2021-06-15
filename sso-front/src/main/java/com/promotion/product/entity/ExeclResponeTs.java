9d9b676568d3f8c1f479180bd79907192d87ab12
lilun <lilun_leo@outlook.com>
2021-06-16 00:36:45 +0800

    修改

--- sso-front/src/main/java/com/promotion/product/entity/ExeclResponeTs.java
+++ sso-front/src/main/java/com/promotion/product/entity/ExeclResponeTs.java
@@ -49,31 +49,31 @@ public class ExeclResponeTs {
     @ExcelColumn(value ="合同收售数量",col = 19)
     private Integer contractAmount;
 
-    @ExcelColumn(value ="预付金额",col = 20)
-    private BigDecimal prepaymentAmount;
+//    @ExcelColumn(value ="预付金额",col = 20)
+//    private BigDecimal prepaymentAmount;
+//
+//    @ExcelColumn(value ="人员费用",col = 21)
+//    private BigDecimal wage;
+//
+//    @ExcelColumn(value ="广告费用",col = 22)
+//    private BigDecimal advertisingFee;
+//
+//    @ExcelColumn(value ="试吃费用",col = 23)
+//    private BigDecimal testFee;
+//
+//    @ExcelColumn(value ="尊享卷费用",col = 24)
+//    private BigDecimal couponFee;
+//
+//    @ExcelColumn(value ="尊享卷费有效期",col = 25)
+//    private String couponEffectiveTime;
+//
+//    @ExcelColumn(value ="折扣费用",col = 26)
+//    private BigDecimal discountFee;
 
-    @ExcelColumn(value ="人员费用",col = 21)
-    private BigDecimal wage;
-
-    @ExcelColumn(value ="广告费用",col = 22)
-    private BigDecimal advertisingFee;
-
-    @ExcelColumn(value ="试吃费用",col = 23)
-    private BigDecimal testFee;
-
-    @ExcelColumn(value ="尊享卷费用",col = 24)
-    private BigDecimal couponFee;
-
-    @ExcelColumn(value ="尊享卷费有效期",col = 25)
-    private String couponEffectiveTime;
-
-    @ExcelColumn(value ="折扣费用",col = 26)
-    private BigDecimal discountFee;
-
-    @ExcelColumn(value ="回款人姓名",col = 27)
+    @ExcelColumn(value ="回款人姓名",col = 20)
     private String billUserName;
 
-    @ExcelColumn(value ="回款人账号",col = 28)
+    @ExcelColumn(value ="回款人账号",col = 21)
     private String billAccountNumber;
 
 
